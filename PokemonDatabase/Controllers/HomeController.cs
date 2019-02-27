42ebd3b8093b4c0b926d9348de0f889eff779f73
Dakota Worzella <dbworzella@students.ntc.edu>
2019-02-27 15:35:19 -0600

    Initial commit of repository

---
+++ PokemonDatabase/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PokemonDatabase.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Hello world!" };
+        }
+    }
+}
