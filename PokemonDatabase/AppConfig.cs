4c2972336ae4001c8c73d427d0e5819f96281903
Dakota Worzella <dbworzella@students.ntc.edu>
2019-02-27 16:08:15 -0600

    Modelled index from LittleAmazon and added Routing

---
+++ PokemonDatabase/AppConfig.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace PokemonDatabase
+{
+    public class AppConfig
+    {
+        public string AppName { get; set; }
+        public string AppVersion { get; set; }
+    }
+}
--- PokemonDatabase/Controllers/HomeController.cs
+++ PokemonDatabase/Controllers/HomeController.cs
@@ -4,14 +4,27 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace PokemonDatabase.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
+        private readonly AppConfig _appConfig;
+
+        public HomeController(IOptions<AppConfig> appConfig)
+        {
+            _appConfig = appConfig.Value;
+        }
+
+        [Route("")]
         public IActionResult Index()
         {
-            return new ContentResult { Content = "Hello world!" };
+            ViewBag.ApplicationName = _appConfig.AppName;
+            ViewBag.ApplicationVersion = _appConfig.AppVersion;
+
+            return View();
         }
     }
 }
