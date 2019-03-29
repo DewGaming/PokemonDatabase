818ac9de348f79d8654ac77b8678b3799e1c34fe
Dakota Worzella <dbworzella@students.ntc.edu>
2019-03-28 19:29:29 -0500

    Added in alolan form images

--- Pokemon Database/PokemonDatabase/wwwroot/js/TabContent.js
+++ Pokemon Database/PokemonDatabase/wwwroot/js/TabContent.js
@@ -5,16 +5,16 @@ function openPage(evt, pageName) {
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].className = tabcontent[i].className.replace(" active", "");
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tab");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  tab = document.getElementsByClassName("tab");
+  for (i = 0; i < tab.length; i++) {
+    tab[i].className = tab[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(pageName).style.display = "block";
+  document.getElementById(pageName).className += " active";
   evt.currentTarget.className += " active";
 } 
