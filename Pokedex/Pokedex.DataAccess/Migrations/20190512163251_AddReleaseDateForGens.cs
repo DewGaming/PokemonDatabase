42a8e62a300b2a7d6ad7ccb39d800b237ab10d40
Dakota Worzella <dbworzella@students.ntc.edu>
2019-05-12 11:56:42 -0500

    Added a release date variable for generations

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20190512163251_AddReleaseDateForGens.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class AddReleaseDateForGens : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Generations",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Generations");
+        }
+    }
+}
--- Pokedex/Pokedex.DataAccess/Models/Generation.cs
+++ Pokedex/Pokedex.DataAccess/Models/Generation.cs
@@ -16,6 +16,9 @@ namespace Pokedex.DataAccess.Models
         [StringLength(5)]
         [Required]
         public string Abbreviation { get; set; }
+        [Display(Name = "Release Date")]
+        [Required]
+        public DateTime ReleaseDate { get; set; }
         [Required]
         public bool IsArchived { get; set; }
     }
