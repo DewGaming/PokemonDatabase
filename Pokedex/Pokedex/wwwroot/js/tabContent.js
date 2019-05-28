7ae527b84d1f5bd0a8fcde7dac3ae0ce3a8561bc
Dakota Worzella <dbworzella@students.ntc.edu>
2019-05-28 18:45:07 -0500

    Modified the tabContent .js file

--- Pokedex/Pokedex/wwwroot/js/tabContent.js
+++ Pokedex/Pokedex/wwwroot/js/tabContent.js
@@ -1,16 +1,16 @@
 function openPage(evt, pageName, username) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].className = tabcontent[i].className.replace(" active", "");
-  }
+  $('.tabcontent').each(function() {
+    $(this).removeClass('active');
+  })
 
-  tab = document.getElementsByClassName("tab");
-  for (i = 0; i < tab.length; i++) {
-    tab[i].className = tab[i].className.replace(" active", "");
-  }
+  $('.tab').each(function() {
+    $(this).removeClass('active');
+  })
 
-  document.getElementById(pageName).className += " active";
-  document.getElementById(username).className += " active";
-  evt.currentTarget.className += " active";
-} 
+  $('#' + pageName).addClass('active');
+  $(evt.currentTarget).addClass('active');
+  if ('#' + username != null)
+  {
+    $('#' + username).addClass('active');
+  }
+}
