fcf5e41843d51cde2c888f47c62bf782f3cf0667
Dakota Worzella <dbworzella@students.ntc.edu>
2019-05-28 00:15:21 -0500

    Added HuntRetried variable

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20190528051448_AddHuntRetryVariable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class AddHuntRetryVariable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HuntRetried",
+                table: "ShinyHunts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HuntRetried",
+                table: "ShinyHunts");
+        }
+    }
+}
