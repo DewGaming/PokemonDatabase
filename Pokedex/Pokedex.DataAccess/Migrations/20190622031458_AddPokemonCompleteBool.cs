8ba0e17df5190b773ba38784e558bc8e50905165
Dakota Worzella <dbworzella@students.ntc.edu>
2019-06-21 23:17:16 -0500

    Added new value to pokemon class

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20190622031458_AddPokemonCompleteBool.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class AddPokemonCompleteBool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsComplete",
+                table: "Pokemon",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsComplete",
+                table: "Pokemon");
+        }
+    }
+}
