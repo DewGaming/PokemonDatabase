36a8194e2f26df350c258267a9fe7044e5d0c390
Dakota Worzella <dbworzella@students.ntc.edu>
2019-08-07 08:42:40 -0500

    Added IsArchived variable to forms

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20190807134208_FormIsArchived.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class FormIsArchived : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "Forms",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "Forms");
+        }
+    }
+}
