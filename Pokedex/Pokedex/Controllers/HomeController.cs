76ca8f1b72ce8ca217540ab3872113dcdeb41f02
Dakota Worzella <dbworzella@students.ntc.edu>
2019-08-23 22:43:22 -0500

    Added an about page

--- Pokedex/Pokedex/Controllers/HomeController.cs
+++ Pokedex/Pokedex/Controllers/HomeController.cs
@@ -681,6 +681,13 @@ namespace Pokedex.Controllers
         }
 
         [AllowAnonymous]
+        [Route("about")]
+        public IActionResult About()
+        {
+            return this.View();
+        }
+
+        [AllowAnonymous]
         [Route("error")]
         public IActionResult Error()
         {
