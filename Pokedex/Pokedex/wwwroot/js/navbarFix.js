efe16dbd227d779db3129e3619bd07a1051d2513
Dakota Worzella <dbworzella@students.ntc.edu>
2019-09-28 00:17:12 -0500

    Changed how the navbar and pokemon page max-width react to window size

---
+++ Pokedex/Pokedex/wwwroot/js/navbarFix.js
@@ -0,0 +1,10 @@
+$(".navbar-toggler").on('click', function (){
+    if($('.fixed-nav').hasClass('nav-scroll'))
+    {
+        $('.fixed-nav').removeClass('nav-scroll');
+    }
+    else
+    {
+        $('.fixed-nav').addClass('nav-scroll');
+    }
+});
