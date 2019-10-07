9039d97f2319e4a997341a4629759fe2370d7fba
Dakota Worzella <dbworzella@students.ntc.edu>
2019-10-07 16:27:29 -0500

    Allows users to create teams without needing to insert a pokemon right away
    (cherry picked from commit 4b1d4a32013c78b3ab0f26a96b928a4c24089385)

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20191007212458_RemovedFirstPokemonNecessityFromTeams.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class RemovedFirstPokemonNecessityFromTeams : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "FirstPokemonId",
+                table: "PokemonTeams",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "FirstPokemonId",
+                table: "PokemonTeams",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
