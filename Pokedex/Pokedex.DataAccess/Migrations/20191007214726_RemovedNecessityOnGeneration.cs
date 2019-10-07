b5080cfcf3b73b0b110e95d0d92fe8ad4b826a4e
Dakota Worzella <dbworzella@students.ntc.edu>
2019-10-07 16:49:25 -0500

    Removed need for generation to be added
    (cherry picked from commit 4358abb27b214ffd9b937afc78e32510da62a15d)

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20191007214726_RemovedNecessityOnGeneration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class RemovedNecessityOnGeneration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GenerationId",
+                table: "PokemonTeams",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GenerationId",
+                table: "PokemonTeams",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
