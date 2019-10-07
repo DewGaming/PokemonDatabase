9281ff9115aed28d679c8a32bc5d2effb3f8caaf
Dakota Worzella <dbworzella@students.ntc.edu>
2019-10-06 20:37:49 -0500

    Fixed Special Ability title & changed pokemon team

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20191006035242_AddVariableToPokemonTeam.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class AddVariableToPokemonTeam : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GenerationId",
+                table: "PokemonTeams",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PokemonTeamName",
+                table: "PokemonTeams",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonTeams_GenerationId",
+                table: "PokemonTeams",
+                column: "GenerationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonTeams_Generations_GenerationId",
+                table: "PokemonTeams",
+                column: "GenerationId",
+                principalTable: "Generations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonTeams_Generations_GenerationId",
+                table: "PokemonTeams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PokemonTeams_GenerationId",
+                table: "PokemonTeams");
+
+            migrationBuilder.DropColumn(
+                name: "GenerationId",
+                table: "PokemonTeams");
+
+            migrationBuilder.DropColumn(
+                name: "PokemonTeamName",
+                table: "PokemonTeams");
+        }
+    }
+}
