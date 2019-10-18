cb7b2738432ed8d3d66dac2f2a152e552361894a
Dakota Worzella <dbworzella@students.ntc.edu>
2019-10-17 21:55:36 -0500

    Upgraded project to ASP.NET Core 2.2

--- Pokedex/Pokedex/Startup.cs
+++ Pokedex/Pokedex/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 using Pokedex.DataAccess.Models;
 
 namespace Pokedex
@@ -22,9 +23,13 @@ namespace Pokedex
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<AppConfig>(Configuration.GetSection("AppConfig"));
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.Configure<AppConfig>(Configuration.GetSection("AppConfig"));
 
             services.AddDbContext<DataContext>(options => options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"))
                 .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
@@ -36,6 +41,8 @@ namespace Pokedex
                     options.LogoutPath = "/logout";
                     options.AccessDeniedPath = "/access_denied";
                 });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
