4cf29c92f2201407752504076ca8f3f0dc2c4004
Dakota Worzella <dbworzella@students.ntc.edu>
2019-10-17 22:11:55 -0500

    Made export buttons hidden until teams are loaded

--- Pokedex/Pokedex/wwwroot/js/pokemonTeam.js
+++ Pokedex/Pokedex/wwwroot/js/pokemonTeam.js
@@ -1,4 +1,58 @@
-var pokemonTeams, exportString;
+var pokemonTeams, exportString, refreshClickEvents = function() {
+    $('.pokemonTeamButton').on("click", function() {
+        var buttonId = $(this).attr('id')
+        if(typeof(pokemonTeams) !== "undefined")
+        {
+            $.each(pokemonTeams, function(index, item) {
+                if(item.teamId == buttonId)
+                {
+                    exportString = item.exportString.replace(':', '\:').replace('(', '\(').replace(')', '\)');
+                }
+            });
+
+            console.clear();
+
+            var temp = $("<textarea>");
+            $("body").append(temp);
+            $(temp).text(exportString);
+            $(temp).select();
+            document.execCommand("copy");
+            $(temp).remove();
+
+            console.log(exportString);
+
+            alert("Teams have been copied to your clipboard!");
+        }
+    });
+
+    $('.pokemonTeamsButton').on("click", function() {
+        if(typeof(pokemonTeams) !== "undefined")
+        {
+            exportString = "";
+            $.each(pokemonTeams, function(index, item) {
+                if(index != 0)
+                {
+                    exportString += "\n\n";
+                }
+                exportString += item.exportString.replace(':', '\:').replace('(', '\(').replace(')', '\)');
+            });
+
+            console.clear();
+
+            var temp = $("<textarea>");
+            $("body").append(temp);
+            $(temp).text(exportString);
+            $(temp).select();
+            document.execCommand("copy");
+            $(temp).remove();
+
+            console.log(exportString);
+
+            alert("Teams have been copied to your clipboard!");
+        }
+    });
+};
+
 $(document).ready(function() {
     $.ajax({
         url: '/grab-all-user-pokemon-teams/',
@@ -6,6 +60,10 @@ $(document).ready(function() {
     })
     .done(function(data) {
         pokemonTeams = data;
+        $('.hide').each(function() {
+            $(this).removeClass('hide');
+        });
+        refreshClickEvents();
     })
     .fail(function(jqXHR) {
         if(jqXHR.statusText != "error")
@@ -14,56 +72,3 @@ $(document).ready(function() {
         }
     });
 })
-
-$('.pokemonTeamButton').on("click", function() {
-    var buttonId = $(this).attr('id')
-    if(typeof(pokemonTeams) !== "undefined")
-    {
-        $.each(pokemonTeams, function(index, item) {
-            if(item.teamId == buttonId)
-            {
-                exportString = item.exportString.replace(':', '\:').replace('(', '\(').replace(')', '\)');
-            }
-        });
-
-        console.clear();
-
-        var temp = $("<textarea>");
-        $("body").append(temp);
-        $(temp).text(exportString);
-        $(temp).select();
-        document.execCommand("copy");
-        $(temp).remove();
-
-        console.log(exportString);
-
-        alert("Teams have been copied to your clipboard!");
-    }
-});
-
-$('.pokemonTeamsButton').on("click", function() {
-    if(typeof(pokemonTeams) !== "undefined")
-    {
-        exportString = "";
-        $.each(pokemonTeams, function(index, item) {
-            if(index != 0)
-            {
-                exportString += "\n\n";
-            }
-            exportString += item.exportString.replace(':', '\:').replace('(', '\(').replace(')', '\)');
-        });
-
-        console.clear();
-
-        var temp = $("<textarea>");
-        $("body").append(temp);
-        $(temp).text(exportString);
-        $(temp).select();
-        document.execCommand("copy");
-        $(temp).remove();
-
-        console.log(exportString);
-
-        alert("Teams have been copied to your clipboard!");
-    }
-});
