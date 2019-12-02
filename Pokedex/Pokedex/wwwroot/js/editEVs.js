403155a07d2173be2ac94929eac04ea3b538d3b2
Dakota Worzella <dbworzella@students.ntc.edu>
2019-12-01 23:04:48 -0600

    Upon ev change, if the ev total exceeds 508, then the updated ev will be changed to largest possible valid number

--- Pokedex/Pokedex/wwwroot/js/editEVs.js
+++ Pokedex/Pokedex/wwwroot/js/editEVs.js
@@ -1,4 +1,44 @@
-var setupLabels = function(health, attack, defense, specialAttack, specialDefense, speed) {
+var checkTotalEVs = function(slider, label)
+{
+    var totalEVs = Number($('.healthSlider').val()) + Number($('.attackSlider').val()) + Number($('.defenseSlider').val()) + Number($('.specialAttackSlider').val()) + Number($('.specialDefenseSlider').val()) + Number($('.speedSlider').val());
+    
+    if(totalEVs > 508)
+    {
+        var otherEVTotal = 0;
+        if(!$(slider).hasClass('healthSlider'))
+        {
+            otherEVTotal += Number($('.healthSlider').val());
+        }
+
+        if(!$(slider).hasClass('attackSlider'))
+        {
+            otherEVTotal += Number($('.attackSlider').val());
+        }
+
+        if(!$(slider).hasClass('defenseSlider'))
+        {
+            otherEVTotal += Number($('.defenseSlider').val());
+        }
+
+        if(!$(slider).hasClass('specialAttackSlider'))
+        {
+            otherEVTotal += Number($('.specialAttackSlider').val());
+        }
+        if(!$(slider).hasClass('specialDefenseSlider'))
+        {
+            otherEVTotal += Number($('.specialDefenseSlider').val());
+        }
+
+        if(!$(slider).hasClass('speedSlider'))
+        {
+            otherEVTotal += Number($('.speedSlider').val());
+        }
+    
+        var remainingEVs = Number(508) - Number(otherEVTotal);
+        $(slider).val(remainingEVs);
+        $(label).text($(slider).val());
+    }
+}, setupLabels = function(health, attack, defense, specialAttack, specialDefense, speed) {
     $('#healthVal').text(health);
     $('#attackVal').text(attack);
     $('#defenseVal').text(defense);
@@ -15,24 +55,30 @@ var setupLabels = function(health, attack, defense, specialAttack, specialDefens
 
 $('.healthSlider').on('input', function() {
     $('#healthVal').text($(this).val());
+    checkTotalEVs($(this), $('#healthVal'));
 });
 
 $('.attackSlider').on('input', function() {
     $('#attackVal').text($(this).val());
+    checkTotalEVs($(this), $('#attackVal'));
 });
 
 $('.defenseSlider').on('input', function() {
     $('#defenseVal').text($(this).val());
+    checkTotalEVs($(this), $('#defenseVal'));
 });
 
 $('.specialAttackSlider').on('input', function() {
     $('#specialAttackVal').text($(this).val());
+    checkTotalEVs($(this), $('#specialAttackVal'));
 });
 
 $('.specialDefenseSlider').on('input', function() {
     $('#specialDefenseVal').text($(this).val());
+    checkTotalEVs($(this), $('#specialDefenseVal'));
 });
 
 $('.speedSlider').on('input', function() {
     $('#speedVal').text($(this).val());
+    checkTotalEVs($(this), $('#speedVal'));
 });
