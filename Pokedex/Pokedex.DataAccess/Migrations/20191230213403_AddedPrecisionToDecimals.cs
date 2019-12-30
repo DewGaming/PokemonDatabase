fc6bafcf3ae69e5f1a313a7b0fe40bd89b19f74b
Dakota Worzella <dbworzella@students.ntc.edu>
2019-12-30 15:35:09 -0600

    The height and weight of a pokemon and the effectiveness of a type against another type are now defined decimals instead of unspecified decimals

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20191230213403_AddedPrecisionToDecimals.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class AddedPrecisionToDecimals : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Effective",
+                table: "TypeCharts",
+                type: "decimal(2,1)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Weight",
+                table: "Pokemon",
+                type: "decimal(4,1)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Height",
+                table: "Pokemon",
+                type: "decimal(3,1)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Effective",
+                table: "TypeCharts",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(2,1)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Weight",
+                table: "Pokemon",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(4,1)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Height",
+                table: "Pokemon",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(3,1)");
+        }
+    }
+}
