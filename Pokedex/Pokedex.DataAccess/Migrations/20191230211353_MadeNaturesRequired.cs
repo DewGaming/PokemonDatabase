69cfb259ccd7d99dbb59bc9fbe9bfe9b2b0ecb59
Dakota Worzella <dbworzella@students.ntc.edu>
2019-12-30 15:34:29 -0600

    Made the nature in a pokemon team's detail required

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20191230211353_MadeNaturesRequired.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class MadeNaturesRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NatureId",
+                table: "PokemonTeamDetails",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NatureId",
+                table: "PokemonTeamDetails",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
