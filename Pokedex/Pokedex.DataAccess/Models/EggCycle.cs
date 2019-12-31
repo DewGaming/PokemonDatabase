ef756e892857df5e2dec2a3e6c1c66398e2232c9
Dakota Worzella <dbworzella@students.ntc.edu>
2019-12-30 21:27:17 -0600

    Egg Cycles will now show the cycle counts with the steps in the pokemon creator page

--- Pokedex/Pokedex.DataAccess/Models/EggCycle.cs
+++ Pokedex/Pokedex.DataAccess/Models/EggCycle.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pokedex.DataAccess.Models
 {
@@ -8,5 +9,13 @@ namespace Pokedex.DataAccess.Models
         public int Id { get; set; }
         [Required, Display(Name = "Egg Cycles")]
         public Int16 CycleCount { get; set; }
+        [Required, NotMapped]
+        public string CycleWithSteps
+        {
+            get
+            {
+                return string.Concat(CycleCount.ToString(), " (", CycleCount * 257, "-", CycleCount * 257 + 256, ")");
+            }
+        }
     }
 }
