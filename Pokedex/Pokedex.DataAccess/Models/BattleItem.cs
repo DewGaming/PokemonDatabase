07f9d4b4c763da4a263080b4bbe6f3b6b135f136
Dakota Worzella <dbworzella@students.ntc.edu>
2020-01-02 20:55:18 -0600

    Swapped Game(s) of Origin for Region of Origin

--- Pokedex/Pokedex.DataAccess/Models/BattleItem.cs
+++ Pokedex/Pokedex.DataAccess/Models/BattleItem.cs
@@ -7,7 +7,7 @@ namespace Pokedex.DataAccess.Models
         public int Id { get; set; }
         [StringLength(20), Required]
         public string Name { get; set; }
-        [Required, Display(Name = "Game(s) of Origin")]
+        [Required, Display(Name = "Region of Origin")]
         public int GenerationId { get; set; }
         public Generation Generation { get; set; }
         [Required, Display(Name = "Only In This Generation")]
