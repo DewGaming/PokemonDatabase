0b47b9b58d2f7e4cf02ea3a06490173fc53615b8
Dakota Worzella <dbworzella@students.ntc.edu>
2020-01-17 22:30:50 -0600

    Removed unneeded code from day care evaluator

--- Pokedex/Pokedex/wwwroot/js/dayCareEvaluator.js
+++ Pokedex/Pokedex/wwwroot/js/dayCareEvaluator.js
@@ -1,16 +1,4 @@
-var fillList = function (data) {
-    var eggGroups = data.allPokemonWithEggGroups, pokemon = data.allPokemon;
-    for (i = 0; i < eggGroups.length; i += 1) {
-        var pokemonDiv = $('<div>'), pokemonLink = $('<a>'), pokemonImage = $('<img>'), pokemonNameDiv = $('<div>'), pokemonName = $('<span>');
-        $(pokemonLink).attr('href', data.appConfig.webUrl + '/pokemon/' + eggGroups[i].pokemon.name.replace(': ', '_').replace(' ', '_').toLowerCase());
-        $(pokemonImage).addClass('pokemonListPicture').attr('title', eggGroups[i].pokemon.name.replace('_', ' ')).attr('src', data.appConfig.webUrl + '/images/pokemon/' + eggGroups[i].pokemon.id + '.png');
-        $(pokemonLink).append(pokemonImage);
-        $(pokemonName).addClass('pokemonName').text(pokemon[i].name.replace('_', ' '));
-        $(pokemonNameDiv).append(pokemonName);
-        $(pokemonDiv).addClass(eggGroups[i].pokemon.name).append(pokemonLink).append(pokemonNameDiv);
-        $('.pokemonList').append(pokemonDiv);
-    };
-}, grabPokemon = function (pokemonId) {
+var grabPokemon = function (pokemonId) {
     $('.pokemonWithEggGroup').empty();
     if (pokemonId != "") {
         $('.pokemonWithEggGroup').load('/get-pokemon-by-egg-group/', { 'pokemonId': pokemonId });
