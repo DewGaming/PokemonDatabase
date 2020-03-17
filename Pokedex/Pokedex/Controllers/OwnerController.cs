b616a7a29e1abd058baa27ee2274e4de294d2fa6
Dakota Worzella <dbworzella@students.ntc.edu>
2020-03-17 17:53:56 -0500

    Users are now organized by Last Visit, then by Id

--- Pokedex/Pokedex/Controllers/OwnerController.cs
+++ Pokedex/Pokedex/Controllers/OwnerController.cs
@@ -28,7 +28,7 @@ namespace Pokedex.Controllers
         {
             UserViewModel model = new UserViewModel()
             {
-                UserList = this.dataService.GetUsers().Where(x => !x.IsOwner).ToList(),
+                UserList = this.dataService.GetUsers().Where(x => !x.IsOwner).OrderBy(x => x.LastVisit).ThenBy(x => x.Id).ToList(),
                 UsersWithShinyHunts = new List<User>(),
                 UsersWithPokemonTeams = new List<User>(),
             };
