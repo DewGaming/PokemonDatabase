a3a9eed36ac8d55ddea7edd488f0c65092cbdbf0
Dakota Worzella <dakotaworzella@gmail.com>
2020-03-17 20:16:16 -0500

    Updated comment class to include newly created category and page classes

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20200318011252_UpdatedCommentClass.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class UpdatedCommentClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentType",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CommentedPage",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Comments",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PageId",
+                table: "Comments",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_CategoryId",
+                table: "Comments",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PageId",
+                table: "Comments",
+                column: "PageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_CommentCategories_CategoryId",
+                table: "Comments",
+                column: "CategoryId",
+                principalTable: "CommentCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_CommentPages_PageId",
+                table: "Comments",
+                column: "PageId",
+                principalTable: "CommentPages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_CommentCategories_CategoryId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_CommentPages_PageId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_CategoryId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_PageId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "PageId",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CommentType",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CommentedPage",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- Pokedex/Pokedex.DataAccess/Models/Comment.cs
+++ Pokedex/Pokedex.DataAccess/Models/Comment.cs
@@ -7,12 +7,13 @@ namespace Pokedex.DataAccess.Models
         public int Id { get; set; }
         [StringLength(500), Required, Display(Name = "Comment")]
         public string Name { get; set; }
-        [Required]
-        public string CommentType { get; set; }
-        public string CommentedPage { get; set; }
+        public int? CategoryId { get; set; }
+        public CommentCategory Category { get; set; }
+        public int? PageId { get; set; }
+        public CommentPage Page { get; set; }
         public string PokemonName { get; set; }
-        public User Commentor { get; set; }
         public int? CommentorId { get; set; }
+        public User Commentor { get; set; }
         [Required]
         public bool IsCompleted { get; set; }
     }
