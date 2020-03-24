de17825c615f2792ea87753ae86b5d1bf745082e
Dakota Worzella <dakotaworzella@gmail.com>
2020-03-24 18:41:01 -0500

    Moved single use javascript files to inline scripts

--- Pokedex/Pokedex/wwwroot/js/navbarFix.js
+++
@@ -1,8 +0,0 @@
-$(".navbar-toggler").on('click', function () {
-    if ($('.fixed-nav').hasClass('nav-scroll')) {
-        $('.fixed-nav').removeClass('nav-scroll');
-    }
-    else {
-        $('.fixed-nav').addClass('nav-scroll');
-    }
-});
