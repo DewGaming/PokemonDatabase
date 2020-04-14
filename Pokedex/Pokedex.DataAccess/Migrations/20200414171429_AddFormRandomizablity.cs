e25f955a856282d8a175548bd350a98f8cb23d6e
Dakota Worzella <dakotaworzella@gmail.com>
2020-04-14 12:31:52 -0500

    Added Randomizable bool to forms

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20200414171429_AddFormRandomizablity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class AddFormRandomizablity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Randomizable",
+                table: "Forms",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Randomizable",
+                table: "Forms");
+        }
+    }
+}
--- Pokedex/Pokedex.DataAccess/Models/Form.cs
+++ Pokedex/Pokedex.DataAccess/Models/Form.cs
@@ -2,10 +2,25 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Pokedex.DataAccess.Models
 {
+    /// <summary>
+    /// The class that is used to represent the form table in a database.
+    /// </summary>
     public class Form   
     {
+        /// <summary>
+        /// Gets or sets the primary key of the table.
+        /// </summary>
         public int Id { get; set; }
+
         [StringLength(25), Required]
+        /// <summary>
+        /// Gets or sets the name of the form.
+        /// </summary>
         public string Name { get; set; }
+
+        [Display(Name="Part Of \"Other Forms\" In Randomize Team Page"), Required]
+        /// <sumnmary>
+        /// Gets or sets the ability to appear in the other forms category in the randomize team page.
+        public bool Randomizable { get; set; }
     }
 }
