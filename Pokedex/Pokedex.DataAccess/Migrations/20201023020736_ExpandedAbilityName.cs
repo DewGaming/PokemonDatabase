831ea4b94fa1d73be8c58d09a505a4d74f73aef3
Dakota Worzella <dakotaworzella@gmail.com>
2020-10-22 21:08:48 -0500

    Expanded Ability Name

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20201023020736_ExpandedAbilityName.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class ExpandedAbilityName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Abilities",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Abilities",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+        }
+    }
+}
--- Pokedex/Pokedex.DataAccess/Models/Ability.cs
+++ Pokedex/Pokedex.DataAccess/Models/Ability.cs
@@ -5,7 +5,7 @@ namespace Pokedex.DataAccess.Models
     public class Ability   
     {
         public int Id { get; set; }
-        [StringLength(20), Required]
+        [StringLength(30), Required]
         public string Name { get; set; }
         [StringLength(300), Required]
         public string Description { get; set; }
