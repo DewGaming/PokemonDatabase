d37b0c58ac804b2e293eada261d20d559cf95b94
Dakota Worzella <dakotaworzella@gmail.com>
2020-10-30 11:10:28 -0500

    Made comment page required and made custom error messages

--- Pokedex/Pokedex.DataAccess/Models/Comment.cs
+++ Pokedex/Pokedex.DataAccess/Models/Comment.cs
@@ -5,11 +5,12 @@ namespace Pokedex.DataAccess.Models
     public class Comment   
     {
         public int Id { get; set; }
-        [StringLength(500), Required, Display(Name = "Comment")]
+        [StringLength(500), Required(ErrorMessage = "Comment is required"), Display(Name = "Comment")]
         public string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Category is required")]
         public int? CategoryId { get; set; }
         public CommentCategory Category { get; set; }
+        [Required(ErrorMessage = "Page is required")]
         public int? PageId { get; set; }
         public CommentPage Page { get; set; }
         public string PokemonName { get; set; }
