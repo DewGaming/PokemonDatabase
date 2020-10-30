b4a7ed29534f1a5b3a21f0c7284a4732fe97c94e
Dakota Worzella <dakotaworzella@gmail.com>
2020-10-30 10:50:27 -0500

    Increased the message's title length

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20201030154957_ElongatedMessageTitleLength.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class ElongatedMessageTitleLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageTitle",
+                table: "Messages",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageTitle",
+                table: "Messages",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000);
+        }
+    }
+}
--- Pokedex/Pokedex.DataAccess/Models/Message.cs
+++ Pokedex/Pokedex.DataAccess/Models/Message.cs
@@ -5,7 +5,7 @@ namespace Pokedex.DataAccess.Models
     public class Message   
     {
         public int Id { get; set; }
-        [StringLength(100), Required, Display(Name = "Title")]
+        [StringLength(1000), Required, Display(Name = "Title")]
         public string MessageTitle { get; set; }
         [StringLength(1000), Required, Display(Name = "Message")]
         public string MessageContent { get; set; }
