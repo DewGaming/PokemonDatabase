1cde3e51c83326312b2f2d1a8649997bcb36e5e1
Dakota Worzella <dakotaworzella@gmail.com>
2021-06-07 17:55:12 -0500

    Introduced the variable GenerationName

--- Pokedex/Pokedex.DataAccess/Models/Generation.cs
+++ Pokedex/Pokedex.DataAccess/Models/Generation.cs
@@ -1,11 +1,14 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pokedex.DataAccess.Models
 {
     public class Generation
     {
         public int Id { get; set; }
+        [Required, NotMapped]
+        public string GenerationName { get { return string.Concat("Generation ", Id); } }
         [Required, StringLength(10)]
         public string Region { get; set; }
     }
