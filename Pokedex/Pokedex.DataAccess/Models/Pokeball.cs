c89f552ad1631c81cf8c98cc9320f61ad4a5a437
Dakota Worzella <dakotaworzella@gmail.com>
2021-06-07 17:57:44 -0500

    Pokeballs now have generation of origin, not region of origin

--- Pokedex/Pokedex.DataAccess/Models/Pokeball.cs
+++ Pokedex/Pokedex.DataAccess/Models/Pokeball.cs
@@ -7,7 +7,7 @@ namespace Pokedex.DataAccess.Models
         public int Id { get; set; }
         [StringLength(20), Required]
         public string Name { get; set; }
-        [Required, Display(Name = "Region Introduced To Normal Gameplay")]
+        [Required, Display(Name = "Generation Introduced To Normal Gameplay")]
         public int GenerationId { get; set; }
         public Generation Generation { get; set; }
     }
