a8a8e4ac9d41ea821ef18aa673c8f177e9eb2844
Dakota Worzella <dakotaworzella@gmail.com>
2021-06-07 19:33:56 -0500

    Regions added to the Games table

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20210608002529_AddedRegionsToGameTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class AddedRegionsToGameTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RegionId",
+                table: "Games",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_RegionId",
+                table: "Games",
+                column: "RegionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Regions_RegionId",
+                table: "Games",
+                column: "RegionId",
+                principalTable: "Regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Regions_RegionId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_RegionId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "RegionId",
+                table: "Games");
+        }
+    }
+}
