0d30bc3029f07019f05774992ba247b94d9e1a39
Dakota Worzella <dakotaworzella@gmail.com>
2021-11-14 11:37:09 -0600

    Added FailedSnag to the PokemonLocationDetail table

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20211114173139_AddedFailedSnagToPokemonLocationDetail.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class AddedFailedSnagToPokemonLocationDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "FailedSnag",
+                table: "PokemonLocationDetails",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedSnag",
+                table: "PokemonLocationDetails");
+        }
+    }
+}
