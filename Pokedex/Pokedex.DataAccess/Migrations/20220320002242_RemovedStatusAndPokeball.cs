d4feaf64f000bee3caf42087c2fd4fcaa2784558
Dakota Worzella <dakotaworzella@gmail.com>
2022-03-19 19:24:00 -0500

    Removed Status and Pokeball from the database

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20220320002242_RemovedStatusAndPokeball.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class RemovedStatusAndPokeball : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PokeballCatchModifierDetails");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+
+            migrationBuilder.DropTable(
+                name: "Pokeballs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pokeballs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenerationId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokeballs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pokeballs_Generations_GenerationId",
+                        column: x => x.GenerationId,
+                        principalTable: "Generations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Effect = table.Column<float>(type: "real", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokeballCatchModifierDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CatchModifier = table.Column<double>(type: "float", nullable: false),
+                    Effect = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PokeballId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokeballCatchModifierDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PokeballCatchModifierDetails_Pokeballs_PokeballId",
+                        column: x => x.PokeballId,
+                        principalTable: "Pokeballs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokeballCatchModifierDetails_PokeballId",
+                table: "PokeballCatchModifierDetails",
+                column: "PokeballId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pokeballs_GenerationId",
+                table: "Pokeballs",
+                column: "GenerationId");
+        }
+    }
+}
