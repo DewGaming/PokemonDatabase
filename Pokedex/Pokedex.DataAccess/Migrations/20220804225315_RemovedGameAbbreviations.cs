e784b09763ff042af871762fe29ca4e227abb9e1
Dakota Worzella <dakotaworzella@gmail.com>
2022-08-04 18:01:07 -0500

    Removed game abbreviations from database

---
+++ Pokedex/Pokedex.DataAccess/Migrations/20220804225315_RemovedGameAbbreviations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.DataAccess.Migrations
+{
+    public partial class RemovedGameAbbreviations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Abbreviation",
+                table: "Games");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Abbreviation",
+                table: "Games",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
